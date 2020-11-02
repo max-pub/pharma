be95cee158ea0105d6ef245e5d8839567ba10df5
Max Fechner <max.fechner@gmail.com>
2020-11-02 10:21:38 +0100

    ...

--- load.category.deno.js
+++ load.category.deno.js
@@ -1,3 +1,6 @@
-import { getPagesFromCategory}  from './lib.js'
+import { getPagesFromCategory } from './lib.js'
 
-getPagesFromCategory("de", "Arzneistoff").then(data=> console.log(JSON.stringify(data, null, 4)))
+let list = await getPagesFromCategory("de", "Arzneistoff")//.then(data=> console.log(JSON.stringify(data, null, 4)))
+
+// Deno.writeTextFileSync('list.json', JSON.stringify(list, null, '\t'))
+Deno.writeTextFileSync('list.txt', list.join('\n'))
