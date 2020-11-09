9f7e235f8eb2655c9460e97041392a1859caad32
Max Fechner <max.fechner@gmail.com>
2020-11-09 11:17:57 +0100

    ....

--- cli/load.languages.deno.js
+++ cli/load.languages.deno.js
@@ -3,13 +3,18 @@ let args = [...Deno.args];
 let titles = args[0]
 let lang = args[1]
 
+let result = await fetch(`https://${lang}.wikipedia.org/w/api.php?action=query&format=json&prop=langlinks&titles=${titles}&llprop=autonym%7Clangname%7Curl`).then(data => data.json())
+// let langlinks = result
+// 	&& result.query
+// 	&& result.query.pages
+// 	&& result.query.pages[Object.keys(result.query.pages)[0]]
+// 	&& result.query.pages[Object.keys(result.query.pages)[0]].langlinks
+// 	? result.query.pages[Object.keys(result.query.pages)[0]].langlinks
+// 	: []
 
-let result = await fetch(`https://${lang}.wikipedia.org/w/api.php?action=query&format=json&prop=langlinks&titles=${titles}&llprop=autonym%7Clangname%7Curl`).then(data=> data.json())
-let langlinks = result
-                &&result.query
-                &&result.query.pages
-                &&result.query.pages[Object.keys(result.query.pages)[0]]
-                &&result.query.pages[Object.keys(result.query.pages)[0]].langlinks
-                ?result.query.pages[Object.keys(result.query.pages)[0]].langlinks
-                :[]
+let langlinks = Object.values(result?.query?.pages)[0]?.langlinks ?? []
 console.log(langlinks)
+
+
+let short = Object.fromEntries(langlinks.map(x => [x.lang, x['*']]));
+console.log(short)
